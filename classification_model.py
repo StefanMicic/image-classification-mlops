5d7f0998645d440c84d88d733f2a3fa6e421a95c
Stefan <stefanmicic8697@gmail.com>
2021-12-25 14:00:34 +0100

    code refactoring

--- classification_model.py
+++ classification_model.py
@@ -1,30 +1,48 @@
+import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
 
 
 class ClassificationModel(keras.Model):
-    def __init__(
-            self
-    ):
-        super(ClassificationModel, self).__init__()
-        self.rescaling = layers.Rescaling(1. / 255)
-        self.conv_1 = layers.Conv2D(32,
-                                    kernel_size=(3, 3),
-                                    activation="relu")
-        self.conv_2 = layers.Conv2D(64,
-                                    kernel_size=(3, 3),
-                                    activation="relu")
-        self.max_pooling = layers.MaxPooling2D(pool_size=(2, 2))
-        self.flatten = layers.Flatten()
-        self.dropout = layers.Dropout(0.5)
-        self.dense = layers.Dense(1, activation="sigmoid")
+    """A model for test classification. It should tell whether the test is positive or not."""
 
-    def call(self, inputs):
-        x = self.rescaling(inputs)
-        x = self.conv_1(x)
-        x = self.max_pooling(x)
-        x = self.conv_2(x)
-        x = self.max_pooling(x)
-        x = self.flatten(x)
-        x = self.dropout(x)
-        return self.dense(x)
+    def __init__(self):
+        """Creates instance of test classification model."""
+        super(ClassificationModel, self).__init__()
+
+        size = 128
+        base_model = keras.applications.MobileNetV2(
+            weights="imagenet",
+            input_shape=(size, size, 3),
+            include_top=False,
+        )
+
+        base_model.trainable = False
+
+        self.base_model = base_model
+        self.avgpool_1 = keras.layers.GlobalAveragePooling2D()
+        self.dense_1 = keras.layers.Dense(256, activation='relu')
+        self.dropout_1 = keras.layers.Dropout(0.2)
+        self.dense_2 = keras.layers.Dense(128, activation='relu')
+        self.dropout_2 = keras.layers.Dropout(0.2)
+        self.dense_3 = keras.layers.Dense(64, activation='relu')
+
+    def call(self, inputs: tf.Tensor) -> tf.Tensor:
+        """
+        Forward pass in neural network.
+
+        Args:
+            inputs: Input tensor.
+
+        Returns:
+            Prediction given by model.
+        """
+        x = self.base_model(inputs)
+        x = self.avgpool_1(x)
+        x = self.dense_1(x)
+
+        x = self.dropout_1(x)
+        x = self.dense_2(x)
+        x = self.dropout_2(x)
+        x = self.dense_3(x)
+
+        return x
